56d1ee885c37a6eaf517af60a64201ee6aa629dd
devasish sai pothumudi <126960187+devasish27@users.noreply.github.com>
2024-08-01 12:08:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MovieList from "./components/MovieList";
+
+function App() {
+  return (
+    <div>
+      <h1>Movie List Application</h1>
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
